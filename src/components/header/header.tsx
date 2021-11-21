cd89b90f194a0627184199a2d4d6206937be4906
Kadin Campbell <campbellkt2013@gmail.com>
2021-11-21 11:08:32 -0500

    working on sidebar within header. Need to make toggleable below 800px and make it more responsive above 1200px

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,51 @@
 import "./header.css";
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
 const Header = () => {
 
-    //from left to right: type of teacher, circular profile picture, # of years teaching 
-
     return(
-        <section>
-            <div>
-                <h1>Teacher type</h1>
+        <section className="header-section">
+            <div className="header-img-container">
+                <img src="https://www.ox.ac.uk/sites/files/oxford/styles/ow_large_feature/s3/field/field_image_main/b_AllSoulsquad.jpg?itok=tTcH-5ix" alt="profile-background" className="profile-background" />
             </div>
-            <div>
-                <h1>profile picture</h1>
+            <div className="header-personal">
+                <div className="profile-img">
+                    <img src="https://media.istockphoto.com/photos/silhouette-of-man-in-dark-place-anonymous-backlit-contour-a-picture-id1139459625?b=1&k=20&m=1139459625&s=170667a&w=0&h=zVpBlAmdwUDWIVf0Zxtb3idMCitol4nzII2qde8Ybag=" alt="profile-img" />
+                </div>
+                <div className="header-personal-detail">
+                    <h1>Kadin Campbell</h1>
+                    <p>Social Studies</p>
+                </div>
             </div>
-            <div>
-                <h1># of years teaching</h1>
-            </div>
+
+            <aside className="sidebar">
+                    <h1>Friends</h1>
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                            <ListItemText primary="Cassidy" />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider />
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                            <ListItemText primary="John" />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider />
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                            <ListItemText primary="Hanh" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+            </aside>
+
         </section>
     )
 }
