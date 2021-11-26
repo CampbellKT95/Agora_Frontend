ad7e0418922a9aefe73e63c788412f5f24262b67
Kadin Campbell <campbellkt2013@gmail.com>
2021-11-25 21:13:27 -0500

    finished liking functionality, working on comments

--- src/components/singleTimeline/singleTimeline.tsx
+++ src/components/singleTimeline/singleTimeline.tsx
@@ -1,5 +1,8 @@
 import "./singleTimeline.css";
-import {useState} from "react";
+import {useState, useContext} from "react";
+import {AuthContext} from "../../context/AuthContext";
+import axios from "axios";
+import Backdrop from '@mui/material/Backdrop';
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import RecommendIcon from '@mui/icons-material/Recommend';
 import ForumIcon from '@mui/icons-material/Forum';
@@ -7,6 +10,38 @@ import ForumIcon from '@mui/icons-material/Forum';
 const SingleTimeline = (props: any) => {
 
     const [displayContent, setDisplayContent] = useState(false);
+    const [likes, setLikes] = useState(props.likes.length)
+    const [numberComments, setNumberComments] = useState(props.comments.length);
+    const [commentsModal, setCommentsModal] = useState(false);
+    const [commentInMaking, setCommentInMaking] = useState("")
+
+    const {user} = useContext(AuthContext);
+
+    const handleLikes = async () => {
+        try {
+            axios.put("/posts/" + props.id + "/like", {userId: user._id})
+
+            let {data} = await axios.get("/posts/" + props.id)
+            
+            setLikes(data.likes.length);
+
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
+    //
+    const handleCommentsSubmit = async (e: any) => {
+        e.preventDefault();
+
+        try {
+
+
+        } catch (err) {
+            console.log(err)
+        }
+    };
+    //
 
     return (
         <div className="timeline-post">
@@ -22,13 +57,32 @@ const SingleTimeline = (props: any) => {
             </div>
             <div className="post-footer-container">
                 <div className="likes-container">
-                    <RecommendIcon className="likes-icon"/>
-                    <p className="number-of-likes">{props.likes}</p>
+                    <RecommendIcon className="likes-icon" 
+                    onClick={handleLikes}/>
+                    <p className="number-of-likes">{likes}</p>
                 </div>
                 <div className="comments-container">
-                    <ForumIcon className="comments-icon"/>
-                    <p className="number-of-comments">{props.comments.length}</p>
+                    <ForumIcon className="comments-icon" onClick={() => setCommentsModal(!commentsModal)}/>
+                    <p className="number-of-comments">{numberComments}</p>
                 </div>
+
+                
+                <Backdrop sx={{color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1}} open={commentsModal}>
+                    <section className="comments-modal-container">
+                        <form onSubmit={() => handleCommentsSubmit} className="comments-form">
+                            <textarea placeholder="What do you have to say?"
+                            value={commentInMaking} onChange={(e) => setCommentInMaking(e.target.value)} />
+                            <button type="submit">Send</button>
+                        </form>
+                        <section className="comments-section">
+                                {props.comments.map((item: string) => {
+                                return <h4>{item}</h4>
+                            })}
+                        </section>
+                    </section>
+                </Backdrop>
+                
+
                 <button className={`${displayContent ? "display-more-btn-clicked" : "display-more-btn"}`}
                 onClick={() => setDisplayContent(!displayContent)}>
                     <ArrowCircleDownIcon sx={{fontSize: 30}}/>
--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,26 +1,29 @@
 import "./timeline.css"
-import {useState, useEffect} from "react";
+import {useState, useEffect, useContext} from "react";
 import axios from "axios";
 import SingleTimeline from "../singleTimeline/singleTimeline";
+import {AuthContext} from "../../context/AuthContext";
 
 const Timeline = () => {
 
     const [posts, setPosts] = useState([]);
 
+    const {user} = useContext(AuthContext)
+
     useEffect(() => {
         const fetchPosts = async () => {
-            const res = await axios.get("http://localhost:5000/api/posts/timeline/619e3a521306c06c25df2661");
+            const res = await axios.get("http://localhost:5000/api/posts/timeline/" + user._id);
 
             setPosts(res.data);
         };
         fetchPosts();
-    }, [])
+    }, [user._id])
 
     return (
         <section className="timeline">
             {posts.map((item: any) => {
                 return <SingleTimeline title={item.title} description={item.description}
-                comments={item.comments} likes={item.likes}
+                comments={item.comments} likes={item.likes} id={item._id}
                 />
             })}
             <button className="load-more-timeline">More</button>
